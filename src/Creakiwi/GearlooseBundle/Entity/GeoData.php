43e21c0e6d3fe2c9f103ba221e6fd1feb3c39adf
pataprout <alexandre@creakiwi.com>
2012-12-17 22:04:06 +0100

    [FEATURE] Started implementation of GeoData entity with Doctrine

--- src/Creakiwi/GearlooseBundle/Entity/GeoData.php
+++ src/Creakiwi/GearlooseBundle/Entity/GeoData.php
@@ -24,45 +24,59 @@ abstract class GeoData
 {
 	/**
 	 * @see http://en.wikipedia.org/wiki/Decimal_degrees
-	 * 
 	 * @var decimal degree
+         * 
+         * @ORM\Column(name="latitude", type="decimal", precision=8, scale=6)
 	 */
 	protected $latitude;
 
 	/**
 	 * @see http://en.wikipedia.org/wiki/Decimal_degrees
-	 * 
 	 * @var decimal degree
+         * 
+         * @ORM\Column(name="longitude", type="decimal", precision=9, scale=6)
 	 */
 	protected $longitude;
 
 	/**
 	 * @var string
+         * 
+         * @ORM\Column(name="address1", type="string")
 	 */
 	protected $address1;
 
 	/**
 	 * @var string
+         * 
+         * @ORM\Column(name="address2", type="string")
 	 */
 	protected $address2;
 
 	/**
 	 * @var string
-	 */
+         * 
+         * @ORM\Column(name="address3", type="string")
+ 	 */
 	protected $address3;
 
 	/**
 	 * @var string
+         * 
+         * @ORM\Column(name="zip_code", type="string", length=16)
 	 */
 	protected $zipCode;
 
 	/**
 	 * @var string
+         * 
+         * @ORM\Column(name="city", type="string", length=64)
 	 */
 	protected $city;
 
 	/**
 	 * @var string
+         * 
+         * @ORM\Column(name="country", type="string", length=2)
 	 */
 	protected $country;
 }
