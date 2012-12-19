53762d20fb7b62426bdf3b6f4e9e69fca53a08cc
pataprout <alexandre@creakiwi.com>
2012-12-19 03:19:30 +0100

    [FEATURE] Implemented DQL to find the closest points stored, giving it latitude, longitude, and a distance in Km

--- src/Creakiwi/GearlooseBundle/Entity/GeoDataRepository.php
+++ src/Creakiwi/GearlooseBundle/Entity/GeoDataRepository.php
@@ -21,6 +21,7 @@ use Doctrine\ORM\EntityRepository;
 class GeoDataRepository extends EntityRepository
 {
     const PI = 3.14159265;
+    const MI2KM = 1.609344;
 
     public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class)
     {
@@ -34,5 +35,43 @@ class GeoDataRepository extends EntityRepository
         //Comments the following line during the validation of the pull-request for PI() function
         //$conf->addCustomNumericFunction('PI', 'DoctrineExtensions\Query\Mysql\Pi');
         $conf->addCustomNumericFunction('SIN', 'DoctrineExtensions\Query\Mysql\Sin');
+    }
+
+    protected function findInArea($latitude, $longitude, $distance)
+    {
+        return $this
+                ->getEntityManager()
+                ->createQuery('
+                    SELECT (
+                        (
+                            ACOS( SIN(:latitude                   * PI() / 180)
+                                * SIN(g.latitude                  * PI() / 180)
+                                + COS(:latitude                   * PI() / 180)
+                                * COS(g.latitude                  * PI() / 180)
+                                * COS((:longitude - g.latitude)   * PI() / 180)
+                            ) * 180 / PI()
+                        ) * 60 * 1.1515 * :mi2km
+                    ) AS distance
+                    FROM GearlooseBundle:GeoData
+                    WHERE distance <= :distance
+                    ORDER BY distance ASC
+                ')
+                ->setParameters(array(
+                    'latitude'  => $latitude,
+                    'longitude' => $longitude,
+                    'distance'  => $distance,
+                    'mi2km'     => static::MI2KM,
+                ))
+        ;
+    }
+
+    public function findAllInArea($latitude, $longitude, $distance = 1)
+    {
+        return $this->findInArea($latitude, $longitude, $distance);
+    }
+
+    public function findOneInArea($latitude, $longitude, $distance = 1)
+    {
+        return $this->findInArea($latitude, $longitude, $distance);
     }
 }
